a8c89d605bb8f70a7c22b7a7ce91d83fbae386c1
chirag0303 <cheekugoyal0303@gmail.com>
2025-07-10 17:41:14 +0530

    FrontEnd-v1

--- FrontEnd/src/Components/NavBar.jsx
+++ FrontEnd/src/Components/NavBar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router"
+import { useMyContext } from "../context/AppContext";
 
 const Navbar = () => {
+
+    const { user, count } = useMyContext();
     return (
         <div className="py-4 px-6 flex justify-between bg-amber-400">
             <div className="font-bold text-white p-2 text-3xl ">Shopping App</div>
@@ -11,9 +14,10 @@ const Navbar = () => {
             <div className="flex gap-2">
                 <Link to='/profile' className="border-1 my-auto py-1 px-1 bg-lime-300">Profile</Link>
                 <Link to='/signup' className="border-1 my-auto py-1 px-1 bg-lime-300">Signup</Link>
+                <p className="border-1 my-auto py-1 px-1 bg-lime-300">Cart: {count}</p>
             </div>
         </div>
     );
 }
 
-export {Navbar};
+export { Navbar };
--- FrontEnd/src/pages/ProfilePage.jsx
+++ FrontEnd/src/pages/ProfilePage.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
-import {Navbar} from "../Components/NavBar"
+import { Navbar } from "../Components/NavBar"
+import { useMyContext } from "../context/AppContext";
 const ProfilePage = () => {
+
+    const { setCount } = useMyContext();
     const [products, setProducts] = useState([]);
+    const [editProductId, setEditProductId] = useState("");
+    const [updatedPrice, setUpdatedPrice] = useState(0);
 
     const getData = async () => {
         try {
-            
+
             const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`, {
                 method: "GET",
             });
@@ -13,24 +18,24 @@ const ProfilePage = () => {
             console.log(result);
             setProducts(result.data.products);
         } catch (err) {
-            console.warn("Error while getting Products: ",err.message);
+            console.warn("Error while getting Products: ", err.message);
         }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getData();
-    },[]);
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
+        try {
             const title = e.target.title.value;
             const price = e.target.price.value;
             const desc = e.target.description.value;
             const quantity = e.target.quantity.value;
 
-            const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`,{
-                method:"POST",
+            const resp = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products`, {
+                method: "POST",
                 body: JSON.stringify({
                     title: title,
                     price: price,
@@ -42,7 +47,7 @@ const ProfilePage = () => {
                 },
             });
 
-            if(resp.status == '201'){
+            if (resp.status == '201') {
                 console.log(resp);
                 alert("Product added");
                 getData();
@@ -57,39 +62,79 @@ const ProfilePage = () => {
         }
     }
 
+    const handleEditProduct = async (productId) => {
+        try {
+            const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/products/${productId}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    price: updatedPrice,
+                }),
+                headers: {
+                    "content-type": "application/json",
+                },
+            });
+            if (res.status === 200) {
+                alert("Product Updated");
+                getData();
+                setEditProductId("");
+            } else {
+                const result = res.json();
+                alert("Error while updating value", result.message);
+            }
+
+        } catch (err) {
+            alert("Cannot update Product");
+            console.log("Cannot update Product", err.message);
+        }
+    }
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div>
                 <form onSubmit={handleSubmit} className="mx-auto my-4 flex flex-col gap-5 p-6 bg-blue-200 max-w-120">
                     <h2 className="self-center font-bold text-xl">Add New Product</h2>
                     <div className="flex gap-4">
                         <label>Title: </label>
-                        <input name="title" type="text" className="b-1 py-1 px-2 rounded-md border-1"/>
+                        <input name="title" type="text" className="b-1 py-1 px-2 rounded-md border-1" />
                     </div>
                     <div className="flex gap-4">
                         <label>Price: </label>
-                        <input name="price" type="number" className="b-1 py-1 px-2 rounded-md border-1"/>
+                        <input name="price" type="number" className="b-1 py-1 px-2 rounded-md border-1" />
                     </div>
                     <div className="flex gap-4">
                         <label>Description: </label>
-                        <input name="description" type="text" className="b-1 py-1 px-2 rounded-md border-1"/>
+                        <input name="description" type="text" className="b-1 py-1 px-2 rounded-md border-1" />
                     </div>
                     <div className="flex gap-4">
                         <label>Quantity: </label>
-                        <input name="quantity" type="number" className="b-1 py-1 px-2 rounded-md border-1"/>
+                        <input name="quantity" type="number" className="b-1 py-1 px-2 rounded-md border-1" />
                     </div>
                     <button className="border-1 py-1 px-2 rounded-md bg-blue-400">Add Product</button>
                 </form>
             </div>
             <div className="flex flex-wrap gap-6 justify-center p-3 ">
-                {products.map((elem)=>{
-                    console.log(elem);
+                {products.map((elem) => {
                     return (
-                        <div key={elem._id} className="p-4 rounded-lg border-1 bg-fuchsia-300">
+                        <div key={elem._id} className="p-4 rounded-lg border-1 bg-yellow-200">
                             <p className="font-bold">{elem.title}</p>
-                            <p>Price: {elem.price}</p>
-                            <p>{elem.description}</p>
+                            {elem._id === editProductId ?
+                                <>
+                                    <div className="flex gap-1">
+                                        <input value={updatedPrice} onChange={(e) => setUpdatedPrice(e.target.value)} className="border-1 py-1 px-2 rounded-md" size="7" />
+                                        <button className="bg-red-400 text-white px-2 py-0.3 rounded hover:bg-red-500 transition duration-200" onClick={() => setEditProductId("")}>Cancel</button>
+                                        <button className="bg-green-500 text-white px-2 py-0.3 rounded hover:bg-green-600 transition duration-200" onClick={() => handleEditProduct(elem._id)}>Save</button>
+                                    </div>
+                                    <p>{elem.description}</p>
+                                </> :
+                                <div >
+                                    <p>Price: {elem.price}</p>
+                                    <p>{elem.description}</p>
+                                    <button onClick={() => { setEditProductId(elem._id); setUpdatedPrice(elem.price) }} className="bg-blue-400 text-white px-2 py-1 rounded hover:bg-blue-500 transition duration-200 mr-2">Edit</button>
+                                    <button onClick={() => { setCount((prev) => prev + 1) }} className="bg-blue-400 text-white px-2 py-1 mt-1 rounded hover:bg-blue-500 transition duration-200">Add to Cart</button>
+                                </div>
+                            }
+
                         </div>
                     );
                 })}
@@ -98,4 +143,4 @@ const ProfilePage = () => {
     );
 }
 
-export {ProfilePage};
+export { ProfilePage };
